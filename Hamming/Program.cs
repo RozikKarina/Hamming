8dd302c43ac8b95a4d59b4502e3ae6a86be31e2e
Karina Rozenboym <karinarozik1@gmail.com>
2024-10-27 02:34:30 +0300

    Добавьте файлы проекта.

---
+++ Hamming/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public static class Hamming
+{
+    public static int Distance(string strand1, string strand2)
+    {
+        if (strand1.Length != strand2.Length)
+        {
+            throw new ArgumentException("The DNA strands must be of equal length.");
+        }
+
+        int hammingDistance = 0;
+
+        for (int i = 0; i < strand1.Length; i++)
+        {
+            if (strand1[i] != strand2[i])
+            {
+                hammingDistance++;
+            }
+        }
+
+        return hammingDistance;
+    }
+}
